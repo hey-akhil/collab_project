826a104db6a509047441fb2f8fd713a936d6a87e
hey-akhil <akhil.pandeyy1@gmail.com>
2025-08-20 12:00:55 +0530

    comp. in num

--- ClientApp/static/js/company_in_number.js
+++ ClientApp/static/js/company_in_number.js
@@ -1,7 +1,7 @@
  // Function to animate numbers
   function animateCounters() {
     const counters = document.querySelectorAll('.counter');
-    const speed = 200; // smaller = faster
+    const speed = 500; // smaller = faster
 
     counters.forEach(counter => {
       const updateCount = () => {
@@ -11,7 +11,7 @@
 
         if (count < target) {
           counter.innerText = Math.ceil(count + increment);
-          setTimeout(updateCount, 20);
+          setTimeout(updateCount, 5);
         } else {
           counter.innerText = target.toLocaleString(); // format with commas
         }
