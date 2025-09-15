da7bfa2cbaa8ddebab3ccfba1e2d0834eff89c56
hey-akhil <akhil.pandeyy1@gmail.com>
2025-09-15 14:22:28 +0530

    fix: resolved duplicate app label issue

--- ClientApp/apps.py
+++ ClientApp/apps.py
@@ -3,6 +3,4 @@ from django.apps import AppConfig
 class ClientappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ClientApp'
-
-    def ready(self):
-        import ClientApp.signals
+    label = 'client_app_label'
